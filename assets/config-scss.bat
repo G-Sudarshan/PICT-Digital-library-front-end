160cc03a24104ebb1fc1f763feeb089bcbd3e295
G Sudarshan's macbook air <11sudarshang@gmail.com>
2021-10-17 15:35:43 +0530

    modified by sudarshan

--- assets/config-scss.bat
+++
@@ -1,2 +0,0 @@
-cd C:\xampp\htdocs\web-components\user_card
-sass --watch scss/style.scss:css/style.css
--- js/sb-admin-2.js
+++ js/sb-admin-2.js
@@ -1,5 +1,21 @@
 (function($) {
   "use strict"; // Start of use strict
+  
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.form-group')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    });
 
   // Toggle the side navigation
   $("#sidebarToggle, #sidebarToggleTop").on('click', function(e) {
